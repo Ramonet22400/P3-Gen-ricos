5d6408e5707b81356df6157c7d159320266633fd
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-19 22:36:56 +0200

    P3 Generics

--- src/EX_02/App.java
+++ src/EX_02/App.java
@@ -1,4 +1,16 @@
 package EX_02;
+// Crea una clase genérica StackArray que utilice un array genérico y un entero que
+//cuente el número de objetos insertados. Tiene que contener los siguientes métodos:
 
 public class App {
+    public static void main(String[] args){
+
+
+
+
+
+
+
+
+    }
 }
